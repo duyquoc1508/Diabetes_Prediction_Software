8c39e36d7ca2934372c9d71eb28019e14f09cea7
duyquoc <duyquoc.nguyen.bk@gmail.com>
2019-05-01 16:11:13 +0700

    diabetes.py

--- diabetes prediction software.py
+++ diabetes prediction software.py
@@ -1,36 +1,50 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+diabetes = pd.read_csv('diabetes.csv')
+print(diabetes.columns) 
+
+diabetes_mod = diabetes[(diabetes.BloodPressure != 0) & (diabetes.BMI != 0) & (diabetes.Glucose != 0)]
+
+feature_names = ['Pregnancies', 'Glucose', 'BloodPressure', 'SkinThickness', 'Insulin', 'BMI', 'DiabetesPedigreeFunction', 'Age']
+X = diabetes_mod[feature_names]
+y = diabetes_mod.Outcome
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, stratify = diabetes_mod.Outcome, random_state=66)
+
+#import modules
 import os
 os.system('color 3f')
-import pandas as pd
-from sklearn.neighbors import KNeighborsClassifier
 
+from sklearn.ensemble import GradientBoostingClassifier
+
+#this module use for speaking
 import win32com.client as wincl
 speak = wincl.Dispatch("SAPI.SpVoice")
 
-data = pd.read_csv('diabetes.csv')
+#select algo
+model = GradientBoostingClassifier()
 
-x = data.iloc[:,[1,2,3,4,5,6,7]]
-y = data.iloc[:,[8]]
+#fit into the model
+model.fit(X_train,y_train)
 
-model = KNeighborsClassifier()
-
-model.fit(x,y)
-
+#oututs
 print('WelCome to Diabetes Prediction Software') #greeting
 speak.Speak('WelCome to Diabetes Prediction Software')
 
-
-print("Enter Glucose, BloodPressure, SkinThickness, Insulin, BMI, DiabetesPedigreeFunction, Age 'with comma'")
-speak.Speak("Enter Glucose, BloodPressure, SkinThickness, Insulin, BMI, DiabetesPedigreeFunction, Age 'with comma'")
+#input format: 2,148,72,35,0,33.6,0.627,50
+print("Enter Pregnancies, Glucose, BloodPressure, SkinThickness, Insulin, BMI, DiabetesPedigreeFunction, Age 'with comma'")
+# speak.Speak("Enter Pregnancies, Glucose, BloodPressure, SkinThickness, Insulin, BMI, DiabetesPedigreeFunction, Age 'with comma'")
 val = list(input().split(","))
-
 pred = model.predict([val])
 print(pred)
 
 if pred == [1]:
     print('You Have Diabetes')
     speak.Speak('You Have Diabetes')
-    print('You need to take this medicines')
-    speak.Speak('You need to take this medicines')
+    print('You need to take this medicine')
+    speak.Speak('You need to take this medicine')
     print('Alpha-glucosidase inhibitors, Biguanides, Dopamine agonist, DPP-4 inhibitors, Meglitinides')
     speak.Speak('Alpha-glucosidase inhibitors, Biguanides, Dopamine agonist, DPP-4 inhibitors, Meglitinides')
     print('And immediate contact to your doctor')
